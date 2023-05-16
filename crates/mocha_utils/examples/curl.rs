2a10ddd11a0a0ec25028b11de10a86e4d0be3aa6
kalmari <tsujianii@tuta.io>
2023-05-16 14:16:28 +0000

    remove redundant parts of examples

--- crates/mocha_utils/examples/curl.rs
+++ crates/mocha_utils/examples/curl.rs
@@ -1,14 +1,9 @@
-use {
-    mocha_utils::{Category, Command, Rule},
-    std::io::ErrorKind,
-};
+use mocha_utils::{Category, Command, Rule};
 
 fn main() {
     let error = Command::new("/usr/bin/curl")
         .arg("https://google.com")
         .execution_policy((Category::Network, Rule::Kill))
-        .execution_policy((Category::Users, ErrorKind::PermissionDenied))
-        .execution_policy((Category::SetUsers, ErrorKind::PermissionDenied))
         .spawn_in_place();
 
     eprintln!("error: {error}");
--- crates/mocha_utils/examples/id.rs
+++ crates/mocha_utils/examples/id.rs
@@ -1,13 +1,11 @@
 use {
-    mocha_utils::{Category, Command, Rule},
+    mocha_utils::{Category, Command},
     std::io::ErrorKind,
 };
 
 fn main() {
     let error = Command::new("/usr/bin/id")
-        .execution_policy((Category::Network, Rule::Kill))
         .execution_policy((Category::Users, ErrorKind::PermissionDenied))
-        .execution_policy((Category::SetUsers, ErrorKind::PermissionDenied))
         .spawn_in_place();
 
     eprintln!("error: {error}");
